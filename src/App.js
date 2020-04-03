9787dd0a307d5a9c2bb2c8b3d6d5646da027acc7
elenawq <60143171+elenawq@users.noreply.github.com>
2020-04-03 18:20:16 +0300

    adding files

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component";
+
+import { SearchBox } from "./components/search-box/search-box.component";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      cats: [],
+      searchField: ""
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ cats: users }));
+  }
+  render() {
+    const { cats, searchField } = this.state;
+    const filteredCats = cats.filter(cat =>
+      cat.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <h1>cats rolodex</h1>
+        <SearchBox
+          placeholder="search cats"
+          handleChange={e => this.setState({ searchField: e.target.value })}
+        />
+        <CardList cats={filteredCats} />
+      </div>
+    );
+  }
+}
+
+export default App;
